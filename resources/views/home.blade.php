05f8ccb29bc05aa0729bef85f8136cdeab351b08
roberto <robertoramirezmoreno@gmail.com>
2023-05-20 03:20:31 +0200

    new icos added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,29 +4,79 @@
 
 @section('content')
 
-<h1 class="custom-heading">Bienvenido al portal de cervecerías</h1>
-<ul>
-    @isset($breweries)
-    @foreach ($breweries as $brewery)
-    <li>{{ $brewery->nombre }} ({{ $brewery->poblacion }})</li>
-    @endforeach
-    @endisset
-</ul>
+<style>
+    .home-container {
+        position: relative;
+        overflow: hidden;
+    }
 
-<a href="{{ route('breweries') }}">
+    .background-video {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        z-index: -1;
+        margin-bottom: 30px;
+    }
+
+    .rounded-image {
+        border-radius: 50%;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+        width: 100%;
+    }
+
+    .enter-icon {
+        color: #FFFFFF;
+        font-size: 30px;
+    }
+
+    body, html {
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+    }
+
+    .full-width {
+        width: 100vw;
+        height: 100vh;
+    }
+
+    .custom-heading {
+        text-decoration: none;
+    }
+</style>
+
+<div class="home-container">
+    <a href="{{ route('breweries') }}">
+        <video autoplay muted loop class="background-video full-width">
+            <source src="{{ asset('videos/cerveza1.mp4') }}" type="video/mp4">
+            Tu navegador no soporta video HTML5.
+        </video>
+    </a>
+
+    <h1 class="custom-heading">Bienvenido al portal de cervecerías</h1>
+    <ul>
+        @isset($breweries)
+        @foreach ($breweries as $brewery)
+        <li>{{ $brewery->nombre }} ({{ $brewery->poblacion }})</li>
+        @endforeach
+        @endisset
+    </ul>
+
     <div class="d-flex justify-content-center align-items-center position-relative">
-      <img src="{{ asset('img/bar.jpg') }}" class="container-fluid opacity-90 shadow rounded" style="width: 100%;">
-      <a href="{{ route('breweries') }}" class="btn btn-danger round-button" style="position: absolute; bottom: 10px; left: 50%; transform: translateX(-50%); background: radial-gradient(#cdb901, #c7066d); width: 75px; height: 75px; border-radius: 50%; display: flex; justify-content: center; align-items: center; box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);">
-        <span style="color: #FFFFFF; font-weight: bold; font-size: 15px;">Entrar</span>
-      </a>
+        <a href="{{ route('breweries') }}">
+            <img src="{{ asset('img/bar12.gif') }}" class="container-fluid opacity-90 shadow rounded-image">
+        </a>
     </div>
-  </a>
-  
-        
-    </div>
-</a>
 
-<h3 class="yellowH2">Aquí encontrarás el mejor catálogo de cervecerías de España</h3>
-<br><br>
+    <div class="enter-button mt-5 d-flex justify-content-center align-items-center" style="margin-bottom: 30px;">
+        <a href="{{ route('breweries') }}" style="color: #FFFFFF;">
+            <i class="enter-icon fas fa-sign-in-alt"></i>
+        </a>
+    </div>
+    
+</div>
 
 @endsection
